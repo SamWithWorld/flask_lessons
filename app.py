9cbc61436ed2f3a6a23c724ff3929cc9a79da663
lovingJune <myjobcareer@163.com>
2022-09-05 06:32:28 +0800

    02_lesson_template

---
+++ app.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # 类
+    class Person(object):
+        name = 'SamMovedOn'
+        age = 18
+
+    # 创建类的实例
+    p = Person()
+
+    context = {
+        'username': 'Sam',
+        'gender': u'男',
+        'age': 17,
+        'person': p,  # 使用实例
+        'websites': {
+            'baidu': 'www.baidu.com',
+            'google': 'www.google.com'
+        }
+    }
+    return render_template('another/index.html', **context)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
