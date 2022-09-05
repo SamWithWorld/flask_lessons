15e1f872f760ce38e13ffaed00dd5fc5713f16d2
lovingJune <myjobcareer@163.com>
2022-09-05 22:32:33 +0800

    04_control_statements_and_filter

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # encoding: utf-8
 '''
-03_lesson_template
+04_control_statements_and_filter
+if 条件判断语句
+for 循环控制语句
+filter 过滤器
 '''
 from flask import Flask, render_template
 
@@ -9,27 +12,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # 类
-    class Person(object):
-        name = 'SamMovedOn'
-        age = 18
-
-    # 创建类的实例
-    p = Person()
-
-    context = {
-        'username': 'Sam',
-        'gender': u'男',
-        'age': 17,
-        'person': p,  # 使用实例
-        'websites': {
-            'baidu': 'www.baidu.com',
-            'google': 'www.google.com'
-        }
+    user = {
+        "name": 'Sam',
+        "age": 25
     }
-    return render_template('another/index.html', **context)
+    books = [
+        {
+            'name': u'西游记',
+            'author': u'吴承恩',
+            'price': 109
+        },
+        {
+            'name': u'红楼梦',
+            'author': u'曹雪芹',
+            'price': 200
+        },
+        {
+            'name': u'三国演义',
+            'author': u'罗贯中',
+            'price': 120
+        },
+        {
+            'name': u'水浒传',
+            'author': u'施耐庵',
+            'price': 130
+        }
+    ]
+
+    fruit = ['apple', 'banana', 'orange']
+
+    return render_template('another/index.html', user=user, books=books, fruit=fruit)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
