f1769d006e630117a763228ea66cc3844632276e
lovingJune <myjobcareer@163.com>
2022-09-08 16:27:09 +0800

    07_database

--- config.py
+++ config.py
@@ -5,3 +5,14 @@
 # @Author    :Sam
 
 SECRET_KEY = '3c43135b-6c84-46cd-a9a9-8c84153291ca'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# 数据库配置信息
+DB_USERNAME = 'root'
+DB_PASSWORD = 'root'
+DB_HOST = '127.0.0.1'
+DB_PORT = '3306'
+DB_DATABASE = 'flask_book_blog'
+DB_URI = 'mysql+pymysql://%s:%s@%s:%s/%s?charset=utf8' % (DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
+
+SQLALCHEMY_DATABASE_URI = DB_URI
